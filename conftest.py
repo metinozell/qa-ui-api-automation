f176cf68ed71983eafd149daa77188ab70b9c2c1
Metin Özel <154251546+metinozell@users.noreply.github.com>
2026-04-02 17:59:27 +0300

    Feat: Page Object Model, Config ve Screenshot özellikleri eklendi

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from config import Config
+import os
+from datetime import datetime
 
 @pytest.fixture
 def driver():
@@ -11,5 +14,30 @@ def driver():
     driver=webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
 
+    driver.get(Config.BASE_URL)
+
     yield driver
     driver.quit()
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    
+    # Eğer test çalıştırıldıysa ve başarısız olduysa (Failed)
+    if rep.when == "call" and rep.failed:
+        mode = "a" if os.path.exists("failures.txt") else "w"
+        try:
+            # Driver'a erişiyoruz
+            if "driver" in item.funcargs:
+                driver = item.funcargs["driver"]
+                # Screenshots klasörü yoksa oluştur
+                if not os.path.exists("screenshots"):
+                    os.makedirs("screenshots")
+                
+                # Dosya adı: test_adı_tarih.png
+                file_name = f"screenshots/{item.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+                driver.save_screenshot(file_name)
+                print(f"\n Hata yakalandı! Ekran görüntüsü kaydedildi: {file_name}")
+        except Exception as e:
+            print(f"Ekran görüntüsü alınırken hata oluştu: {e}")
--- tests/test_ui_login.py
+++ tests/test_ui_login.py
@@ -1,11 +1,12 @@
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
+from config import Config
 
 def test_succesful_login_with_pom(driver):
     driver.get("https://saucedemo.com/")
     login_pg=LoginPage(driver)
-    login_pg.enter_username("standard_user")
-    login_pg.enter_password("secret_sauce")
+    login_pg.enter_username(Config.USERNAME)
+    login_pg.enter_password(Config.PASSWORD)
     login_pg.click_login()
 
     assert "inventory.html" in driver.current_url
@@ -14,10 +15,10 @@ def test_add_to_cart_with_pom(driver):
     driver.get("https://saucedemo.com/")
 
     login_pg=LoginPage(driver)
-    login_pg.enter_username("standard_user")
-    login_pg.enter_password("secret_sauce")
+    login_pg.enter_username(Config.USERNAME)
+    login_pg.enter_password(Config.PASSWORD)
     login_pg.click_login()
-    
+
     inventory_pg=InventoryPage(driver)
     inventory_pg.add_backpack_to_cart()
     assert inventory_pg.get_cart_count()=="1"
